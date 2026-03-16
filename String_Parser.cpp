ccdc000ab54449d70315ea7baded64233838b375
Isaac Gener <icegen112@gmail.com>
2026-03-15 22:13:05 -0700

    Code changes complete

--- String_Parser.cpp
+++ String_Parser.cpp
@@ -11,3 +11,30 @@ int main() {
 
    return 0;
 }
+
+void parseCSV(const string& students) {
+    ifstream file(students);
+    string line;
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+
+        string name; 
+        string age;
+        string initial; 
+        string isStudent;
+        string grade;
+
+        getline(ss, name, ' ');
+        getline(ss, age, ' ');
+        getline(ss, initial, ' ');
+        getline(ss, isStudent, ' ');
+        getline(ss, grade, ' ');
+
+        cout << name << endl;
+        cout << age << endl;
+        cout << initial << endl;
+        cout << isStudent << endl;
+        cout << grade << endl << endl;
+    }
+}
